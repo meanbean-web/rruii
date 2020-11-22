a1dec620dfcd3de521548e0080f469edc4c17021
Andreea Bunica <andreeabunica1997@gmail.com>
2020-11-22 17:55:49 +0200

    second version

---
+++ app.js
@@ -0,0 +1,39 @@
+//import * as THREE from "three";
+
+class Sketch {
+    constructor() {
+        this.renderer= new THREE.WebGLRenderer( {antialias: true}); 
+        this.renderer.setsize (window.innerWidth, window.innerHeight);
+        document.getElementById('container').appendChild(this.renderer.domElement); 
+        
+        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+        this.camera.position.z = 1;
+
+        this.scene = new THREE.Scene();
+
+        this.addMesh();
+        this.time = 0;
+        this.render();
+
+    }
+
+    addMesh(){
+        this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+        this.material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
+        this.mesh= new THREE.Mesh(this.geometry, this.material);
+        this.scene.add(this.mesh);
+
+    }
+
+    render() {
+        this.time++;
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        console.log(this.time);
+        this.renderer.render(this.scene, this.camera);
+        window.requestAnimationFrame(this.render.bind);
+
+    }
+}
+
+new Sketch();
